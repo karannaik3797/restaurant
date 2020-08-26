2c27634a01ec5037326dedab8f98421986abcf1b
Karan Naik <karannaik3797@gmail.com>
2020-08-26 18:43:18 +0530

    RestaurantApp1

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	'secretKey': '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://localhost:27017/conFusion',
+	'secretKey': 'Your secretKey',
+	'mongoUrl': 'Databse URL',
 	'facebook':{
-		clientId:3444617878927446,
-		clientSecret:'bba9c244e5bad103dd26607a650871bb'
+		clientId:"Your clientID",
+		clientSecret:'Your clientSecret'
 	}
 }
